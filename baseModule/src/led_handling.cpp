575948263be082b94e254bf93180d1c488e4d505
Luca Kannenberg <lucakannenberg@gmail.com>
2023-10-17 15:09:48 +0200

    Very rough structure for the webserver- and led-handling

---
+++ baseModule/src/led_handling.cpp
@@ -0,0 +1,38 @@
+#include "led_handling.h"
+
+
+void led_map_init(){
+
+}
+
+void init_leds(){
+
+}
+
+void buildLEDConnection(){
+
+}
+
+uint8_t getNumOfDigits(uint16_t number){
+
+}
+
+void project_number(uint16_t number){
+
+}
+
+void project_multiple_chars(uint8_t num_of_chars, uint8_t* char_list, uint8_t* x_offset_list, uint8_t y_offset){
+
+}
+
+void allLEDSoff(){
+
+}
+
+void backGroundEvent(){
+
+}
+
+void adjustBrightness(uint16_t brightness){
+
+}
---
+++ baseModule/src/webserver_handling.cpp
@@ -0,0 +1,16 @@
+#include "webserver_handling.h"
+#include "webpage.h"
+#include "internet_settings.h"
+#include <ESP8266WiFi.h>
+
+void buildRouterConnection(){
+
+}
+
+void handleHTMLRequest(AsyncWebServerRequest *request){
+
+}
+
+void handleJSONRequest(AsyncWebServerRequest *request){
+
+}
