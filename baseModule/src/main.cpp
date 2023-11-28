b014cc48557c032f5d1a265d8fb9b0a2a468249a
Luca Kannenberg <lucakannenberg@gmail.com>
2023-11-28 18:45:33 +0100

    rough working background animation,  but WIP

--- baseModule/src/main.cpp
+++ baseModule/src/main.cpp
@@ -8,43 +8,37 @@ String serialInput;
 void setup() {
     Serial.begin(9600);
     Serial.println("Starting...");
-    buildIrConnection();
-    if(false){ // to lazy for feature branch, this will do
-        buildRouterConnection();
-        buildTimeConnection();
-        buildLedConnection();
-        initialiseLedMap();
-        clearActiveLeds();
-    }
+    buildRouterConnection();
+    buildTimeConnection();
+    buildLedConnection();
+    initialiseLedMap();
+    clearActiveLeds();
 }
 
 void loop() {
-    uint16_t irData = decodeIR();
-
-    if(false){
-        if(Serial.available() > 0){
-            clearActiveLeds();
-            serialInput = Serial.readStringUntil('\n');
-            if(serialInput.equals("bg")){
-                // toggle background
-                Serial.println("background");
-            } else if (serialInput.equals("ra")) {
-                // print example string message
-                Serial.println("Example String");
-                projectExampleString();
+    if(Serial.available() > 0){
+        clearActiveLeds();
+        serialInput = Serial.readStringUntil('\n');
+        if(serialInput.equals("bg")){
+            // toggle background
+            Serial.println("background");
+            backgroundEvent();
+        } else if (serialInput.equals("ra")) {
+            // print example string message
+            Serial.println("Example String");
+            projectExampleString();
+        } else {
+            Serial.println("got number");
+            uint16_t serialNumber = serialInput.toInt() % 65535;
+            if(serialNumber) {
+                Serial.printf("got number %d", serialNumber);
+                projectNumber(serialNumber);
             } else {
-                Serial.println("got number");
-                uint16_t serialNumber = serialInput.toInt() % 65535;
-                if(serialNumber) {
-                    Serial.printf("got number %d", serialNumber);
-                    projectNumber(serialNumber);
-                } else {
-                    Serial.println("projecting time");
-                    struct simpleTime * currentTime = (struct simpleTime*)malloc(sizeof(struct simpleTime));
-                    getSimpleTime(currentTime);
-                    projectTime(currentTime->hour, currentTime->minute);
-                    free(currentTime);
-                }
+                Serial.println("projecting time");
+                struct simpleTime * currentTime = (struct simpleTime*)malloc(sizeof(struct simpleTime));
+                getSimpleTime(currentTime);
+                projectTime(currentTime->hour, currentTime->minute);
+                free(currentTime);
             }
         }
     }
