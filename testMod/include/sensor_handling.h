dca5f0c61028e191b5f357d981fcead2327a1e4f
Luca Kannenberg <lucakannenberg@gmail.com>
2024-04-19 22:47:16 +0200

    i should split this into more commits ...

---
+++ testMod/include/sensor_handling.h
@@ -0,0 +1,68 @@
+// Handles Connection to
+// -> BME680
+// -> Photoresitor
+#ifndef SENSOR_HANDLING
+#define SENSOR_HANDLING
+
+#include <inttypes.h>
+#include <ArduinoJson.h>
+
+
+// Maximum Number of concurrent Sensor Readings that will be stored at any time
+// the handleJson function fails after 79 elements(will send 79 elements without
+// the closing ]
+// will investigate why at some point
+#define NUM_READINGS 60
+
+
+
+
+/*
+    Simple Implementation of a ring array by storing number of current Readings and start index of the list
+*/
+
+void initSensor();
+void getSensorReading(char* formattedResponse, size_t maxResponseLen);
+void getSensorReadingFromList(char* formattedResponse, size_t maxResponseLen, uint8_t listIndex);
+
+
+/**
+ * @brief
+ *
+ */
+void buildSensorConnection();
+
+/**
+ * @brief
+ *
+ */
+bool updateSensorValues();
+
+/**
+ * @brief
+ *
+ * @return int
+ */
+int addReadingToList();
+
+/**
+ * @brief Get the Num Of Readings In List object
+ *
+ * @return int
+ */
+int getNumOfReadingsInList();
+
+/**
+ * @brief
+ *
+ * @return int
+ */
+int readAtIndex();
+
+/**
+ * @brief Used for debugging, prints current values the Sensor sends back
+ *
+ */
+void printCurrentReading();
+
+#endif
