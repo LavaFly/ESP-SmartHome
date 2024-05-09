31efdd610906f7d6f6d3a834e5fb5b569874133d
Luca Kannenberg <lucakannenberg@gmail.com>
2024-05-09 17:17:46 +0200

    a working integration of the vr into the module, will rework this someday

--- ledModule/src/main.cpp
+++ ledModule/src/main.cpp
@@ -5,6 +5,7 @@
 #include "time_handling.h"
 #include "ir_handling.h"
 #include "weather_handling.h"
+#include "voice_handling.h"
 
 String serialInput;
 uint8_t irInput;
@@ -13,12 +14,29 @@ uint8_t irInput;
 
 unsigned long currentTime = 0;
 unsigned long clearTimer = 0;
+unsigned long vrClearTimer = 0;
 
 
 bool loopActive = false;
 bool animationActive = false;
 bool toBeCleared = false;
 
+
+// 1 = PC, 2 = Light
+uint8_t vrTreeBranch = 0;
+void setupEventResponse();
+
+void vr_pc();
+void vr_light();
+void vr_time();
+void vr_weather();
+void vr_temperature();
+void vr_co2();
+void vr_on();
+void vr_off();
+void vr_brighter();
+void vr_darker();
+void vr_empty();
 
 void setup() {
     //Serial.begin(9600,SERIAL_8N1,SERIAL_TX_ONLY); // to limit inbound serial comminucation from interefering
@@ -29,6 +47,7 @@ void setup() {
     /**
     buildIrConnection();
     **/
+    initVR();
     buildRouterConnection();
     buildTimeConnection();
     initWebserver();
@@ -39,6 +58,8 @@ void setup() {
     initialiseLedMap();
     clearActiveLeds();
 
+    setupEventResponse();
+
     currentTime = millis();
     Serial.println("finishing the setup");
 }
@@ -46,6 +67,7 @@ void setup() {
 void loop() {
     MDNS.update();
     loopOTA();
+    handleVR();
 
     cleanUpSockets();
     if(animationActive && millis() > currentTime + 150){
@@ -194,4 +216,90 @@ void setupTextAnimation(String message){
     uint8_t num = serialInput.length();
     startSlideAnimation(serialInput.c_str(), num);
     animationActive = true;
+}
+
+void setupEventResponse(){
+    addToEventReponse(0, vr_pc);
+    addToEventReponse(1, vr_light);
+    addToEventReponse(2, vr_time);
+    addToEventReponse(3, vr_weather);
+    addToEventReponse(4, vr_temperature);
+    addToEventReponse(5, vr_co2);
+    addToEventReponse(6, vr_empty);
+    addToEventReponse(7, vr_empty);
+    addToEventReponse(8, vr_on);
+    addToEventReponse(9, vr_off);
+    addToEventReponse(10, vr_brighter);
+    addToEventReponse(11, vr_darker);
+}
+
+void vr_pc(){
+    vrTreeBranch = 1;
+    loadOnOff();
+}
+void vr_light(){
+    vrTreeBranch = 2;
+    loadOnOffBrighterDarker();
+}
+void vr_time(){
+    Serial.println("");
+    showTime();
+}
+void vr_weather(){
+    Serial.println("");
+
+}
+void vr_temperature(){
+    Serial.println("");
+
+}
+void vr_co2(){
+    Serial.println("");
+
+}
+void vr_on(){
+    Serial.println("");
+    if(vrTreeBranch == 1){
+        httpGetRequestIgnoreResponse("http://lightingModule.local/pcPowerOn");
+    } else if(vrTreeBranch == 2){
+        httpGetRequestIgnoreResponse("http://lightingModule.local/lightingOn");
+    } else {
+
+    }
+    loadDefaultVR();
+}
+void vr_off(){
+    Serial.println("");
+    if(vrTreeBranch == 1){
+        Serial.println("not properly implemented, as i havent spliced the necessary cable yet");
+        httpGetRequestIgnoreResponse("http://lightingModule.local/pcPowerOn");
+    } else if(vrTreeBranch == 2){
+        httpGetRequestIgnoreResponse("http://lightingModule.local/lightingOff");
+    } else {
+
+    }
+    loadDefaultVR();
+}
+void vr_brighter(){
+    if(vrTreeBranch == 1){
+        Serial.println("impossible");
+    } else if(vrTreeBranch == 2){
+        httpGetRequestIgnoreResponse("http://lightingModule.local/raiseBrightness");
+    } else {
+
+    }
+    loadDefaultVR();
+}
+void vr_darker(){
+    if(vrTreeBranch == 1){
+        Serial.println("impossible");
+    } else if(vrTreeBranch == 2){
+        httpGetRequestIgnoreResponse("http://lightingModule.local/lowerBrightness");
+    } else {
+
+    }
+    loadDefaultVR();
+}
+void vr_empty(){
+    loadDefaultVR();
 }
