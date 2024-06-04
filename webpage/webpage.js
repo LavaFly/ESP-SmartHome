a9766255d839d991260b52a418a102b4843e5e77
Luca Kannenberg <lucakannenberg@gmail.com>
2024-06-05 00:50:16 +0200

    some work on the webpage

--- webpage/webpage.js
+++ webpage/webpage.js
@@ -42,42 +42,29 @@ window.onload = function () {
         animationEnabled: true,
         backgroundColor: "",
         title: {
-            text: "BME680 Measurements"
+            text: "Overview of the System"
         },
         axisX: {
             lableAngle: -50,
             valueFormatString: "DD HH:mm",
             labelFontColor: "#fffff0"
         },
-        axisY: [{
+        axisY: {
             title: "Temperature",
             suffix: "°C",
             lineColor: "#CD7045",
             tickColor: "#CD7045",
             labelFontColor: "#CD7045"
-        },
-        {
+        }
+        ,
+        axisY2: {
             title: "Humidity",
             suffix: "%",
             lineColor: "#00BFFF",
             tickColor: "#00BFFF",
             labelFontColor: "#00BFFF"
-        }],
-        axisY2: [{
-            title: "Quality",
-            suffix: "%",
-            lineColor: "#A9A9A9",
-            tickColor: "#A9A9A9",
-            labelFontColor: "#A9A9A9"
-        },
-        {
-            title: "Brightness",
-            suffix: "%",
-            lineColor: "#FF8000",
-            tickColor: "#FF8000",
-            labelFontColor: "#FF8000"
         }
-        ],
+        ,
         toolTip: {
             shared: true
         },
@@ -89,7 +76,6 @@ window.onload = function () {
             type: "spline",
             name: "Temperature",
             connectNullData: true,
-            //nullDataLineDashType: "solid",
             xValueType: "dateTime",
             xValueFormatString: "HH:mm DD/MMMM",
             yValueFormatString: "#,###",
@@ -109,12 +95,12 @@ window.onload = function () {
         },
         {
             type: "spline",
-            name: "Quality",
+            name: "CO2",
             connectNullData: true,
             axisYType: "secondary",
             showInLegend: true,
             color: "#A9A9A9",
-            dataPoints: resistance_data
+            dataPoints: co2_data
         },
         {
             type: "spline",
@@ -131,7 +117,7 @@ window.onload = function () {
         animationEnabled: true,
         backgroundColor: "",
         title: {
-            text: "BME680 Measurements"
+            text: "Base Module"
         },
         axisX: {
             lableAngle: -50,
@@ -215,7 +201,66 @@ window.onload = function () {
             dataPoints: brightness_data
         }]
     });
-    var temperatureChart = new CanvasJS.Chart("temperatureChart", {
+    var pcChart = new CanvasJS.Chart("pcChart", {
+        //exportEnabled: true,
+        animationEnabled: true,
+        backgroundColor: "",
+        title: {
+            text: "PC Module"
+        },
+        axisX: {
+            lableAngle: -50,
+            valueFormatString: "DD HH:mm",
+            labelFontColor: "#fffff0"
+        },
+        axisY: [{
+            title: "Temperature",
+            suffix: "°C",
+            lineColor: "#CD7045",
+            tickColor: "#CD7045",
+            labelFontColor: "#CD7045"
+        }
+        ],
+        axisY2: [{
+            title: "Humidity",
+            suffix: "°C",
+            lineColor: "#00BFFF",
+            tickColor: "#00BFFF",
+            labelFontColor: "#00BFFF"
+        }],
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+            type: "spline",
+            name: "BME680",
+            connectNullData: true,
+            //nullDataLineDashType: "solid",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm DD/MMMM",
+            yValueFormatString: "#,###",
+            axisYIndex: 0,
+            showInLegend: true,
+            color: "#CD7045",
+            dataPoints: temperaturePC_data
+        },
+        {
+            type: "spline",
+            name: "DHT22",
+            connectNullData: true,
+            axisYIndex: 1,
+            axisYType: "secondary",
+            showInLegend: true,
+            color: "#00BFFF",
+            dataPoints: humidityPC_data
+        }]
+    });
+
+    var sensorChart = new CanvasJS.Chart("sensorChart", {
         //exportEnabled: true,
         animationEnabled: true,
         backgroundColor: "",
@@ -233,8 +278,9 @@ window.onload = function () {
             lineColor: "#CD7045",
             tickColor: "#CD7045",
             labelFontColor: "#CD7045"
-        },
-        {
+        }
+        ],
+        axisY2: [{
             title: "PC Temperature",
             suffix: "°C",
             lineColor: "#00BFFF",
@@ -266,6 +312,7 @@ window.onload = function () {
             name: "DHT22",
             connectNullData: true,
             axisYIndex: 1,
+            axisYType: "secondary",
             showInLegend: true,
             color: "#00BFFF",
             dataPoints: temperaturePC_data
@@ -289,8 +336,9 @@ window.onload = function () {
             lineColor: "#CD7045",
             tickColor: "#CD7045",
             labelFontColor: "#CD7045"
-        },
-        {
+        }
+        ],
+        axisY2: [{
             title: "Air Resistance",
             suffix: "",
             lineColor: "#00BFFF",
@@ -320,6 +368,7 @@ window.onload = function () {
             type: "spline",
             name: "Resistance",
             connectNullData: true,
+            axisYType: "secondary",
             axisYIndex: 1,
             showInLegend: true,
             color: "#00BFFF",
@@ -410,8 +459,9 @@ window.onload = function () {
     function renderCharts() {
         bigChart.render();
         baseChart.render();
-        temperatureChart.render();
+        sensorChart.render();
         airqualityChart.render();
+        pcChart.render();
     }
     checkWebsiteStatus('http://baseModule.local/isLive', document.getElementById('baseStatus'));
     checkWebsiteStatus('http://ledModule.local/isLive', document.getElementById('ledStatus'));
