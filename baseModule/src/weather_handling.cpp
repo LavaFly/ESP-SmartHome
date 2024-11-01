16d3897e6caed96f81a9f3849cef4be72ed7ce69
Luca Kannenberg <lucakannenberg@gmail.com>
2024-11-01 22:16:23 +0100

    wtf happend here

--- baseModule/src/weather_handling.cpp
+++ baseModule/src/weather_handling.cpp
@@ -5,7 +5,11 @@
 #include "webserver_handling.h"
 #include "weather_handling.h"
 
+/**
+ * wtf happend here, why did i do this?
+ * need to investigate at some point
 JsonDocument doc;
+
 void printWeatherData(){
     // for now, location is fixed, later changeable through WebUI?
     // API is defined in internet_settings which is included in webserver_handling
@@ -29,4 +33,62 @@ void printWeatherData(){
     Serial.println(tmp);
     Serial.print("humitity: ");
     Serial.println(humid);
+
+    doc.clear();
 }
+
+void printForecastData(){
+    char path[120] = "http://api.openweathermap.org/data/2.5/forecast?lat=49.0047&lon=8.3858&units=metric&appid=";
+    char completePath[128];
+    strcpy(completePath, path);
+    strcat(completePath, API);
+    JsonDocument filter;
+    filter["list"][0]["dt"] = true;
+    filter["list"][0]["main"]["temp"] = true;
+    filter["list"][0]["weather"][0]["description"] = true;
+
+    DeserializationError err = deserializeJson(doc, httpGetRequestStream(completePath), DeserializationOption::Filter(filter));
+    httpEndRequestStream();
+
+    if(err.Ok){
+        Serial.println(err.c_str());
+        Serial.println(err.code());
+        Serial.println("fuck");
+        return;
+    }
+
+    Serial.println(doc["list"][3]["dt"].as<long>());
+    Serial.println(doc["list"][3]["main"]["temp"].as<float>());
+    Serial.println(doc["list"][3]["weather"][0]["description"].as<String>());
+
+
+    doc.clear();
+}
+
+String getForecastData(){
+    char path[120] = "http://api.openweathermap.org/data/2.5/forecast?lat=49.0047&lon=8.3858&units=metric&appid=";
+    char completePath[128];
+    strcpy(completePath, path);
+    strcat(completePath, API);
+    JsonDocument filter;
+    filter["list"][0]["dt"] = true;
+    filter["list"][0]["main"]["temp"] = true;
+    filter["list"][0]["weather"][0]["description"] = true;
+
+    DeserializationError err = deserializeJson(doc, httpGetRequestStream(completePath), DeserializationOption::Filter(filter));
+    httpEndRequestStream();
+
+    if(err.Ok){
+        Serial.println(err.c_str());
+        Serial.println(err.code());
+        Serial.println("fuck");
+        return;
+    }
+
+    Serial.println(doc["list"][3]["dt"].as<long>());
+    Serial.println(doc["list"][3]["main"]["temp"].as<float>());
+    Serial.println(doc["list"][3]["weather"][0]["description"].as<String>());
+
+    doc.clear();
+}
+**/
