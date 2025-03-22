32083e314385cef1be1bfe50e06ff5cd92157bb6
Luca Kannenberg <lucakannenberg@gmail.com>
2025-03-22 20:18:35 +0100

    better webpage

--- baseModule/website/webpage.js
+++ baseModule/website/webpage.js
@@ -27,21 +27,20 @@ function sendGetRequest(module, endpoint) {
     xhr.send();
 }
 window.onload = function () {
-    var temperatureBase_data = [];
-    var temperaturePC_data = [];
-    var humidityBase_data = [];
-    var humidityPC_data = [];
-    var co2_data = []
-    var resistance_data = [];
-    var brightness_data = [];
+    var baseData = { temperature: [], humidity: [], co2: [], brightness: [] }
+    var tentData = { temperature: [], humidity: [], co2: [], brightness: [], resistance: [] }
+    var pcData = { temperature: [], humidity: [] }
+
     var pathToBase = "http://localhost:8080/website/mock_sensor.json" // http://baseModule.local/json
+    var pathToTent = "http://localhost:8080/website/mock_sensor.json" // http://tentModule.local/json
     var pathToPc = "http://localhost:8080/website/mock_sensor.json" // http://pcModule.local/json
-    var bigChart = new CanvasJS.Chart("bigChart", {
+
+    var baseChart = new CanvasJS.Chart("baseChart", {
         //exportEnabled: true,
         animationEnabled: true,
         backgroundColor: "",
         title: {
-            text: "Overview of the System",
+            text: "Room Stats",
             fontColor: "#999999"
         },
         axisX: {
@@ -85,7 +84,7 @@ window.onload = function () {
             axisYIndex: 0,
             showInLegend: true,
             color: "#CD7045",
-            dataPoints: temperatureBase_data
+            dataPoints: baseData.temperature
         },
         {
             type: "spline",
@@ -94,7 +93,7 @@ window.onload = function () {
             axisYIndex: 1,
             showInLegend: true,
             color: "#00BFFF",
-            dataPoints: humidityBase_data
+            dataPoints: baseData.humidity
         },
         {
             type: "spline",
@@ -103,7 +102,7 @@ window.onload = function () {
             axisYType: "secondary",
             showInLegend: true,
             color: "#A9A9A9",
-            dataPoints: co2_data
+            dataPoints: baseData.co2
         },
         {
             type: "spline",
@@ -112,14 +111,14 @@ window.onload = function () {
             axisYType: "secondary",
             showInLegend: true,
             color: "yellow",
-            dataPoints: brightness_data
+            dataPoints: baseData.brightness
         }],
         options: {
             responsive: true,
             maintainAspectRatio: false
         }
     });
-    var baseChart = new CanvasJS.Chart("baseChart", {
+    var tentChart = new CanvasJS.Chart("tentChart", {
         //exportEnabled: true,
         animationEnabled: true,
         backgroundColor: "",
@@ -188,7 +187,7 @@ window.onload = function () {
             axisYIndex: 0,
             showInLegend: true,
             color: "#CD7045",
-            dataPoints: temperatureBase_data
+            dataPoints: tentData.temperature
         },
         {
             type: "spline",
@@ -197,7 +196,7 @@ window.onload = function () {
             axisYIndex: 1,
             showInLegend: true,
             color: "#00BFFF",
-            dataPoints: humidityBase_data
+            dataPoints: tentData.humidity
         },
         {
             type: "spline",
@@ -206,7 +205,7 @@ window.onload = function () {
             axisYType: "secondary",
             showInLegend: true,
             color: "#A9A9A9",
-            dataPoints: resistance_data
+            dataPoints: tentData.resistance
         },
         {
             type: "spline",
@@ -215,7 +214,7 @@ window.onload = function () {
             axisYType: "secondary",
             showInLegend: true,
             color: "yellow",
-            dataPoints: brightness_data
+            dataPoints: tentData.brightness
         }],
         options: {
             responsive: true,
@@ -271,7 +270,7 @@ window.onload = function () {
             axisYIndex: 0,
             showInLegend: true,
             color: "#993300",
-            dataPoints: temperaturePC_data
+            dataPoints: pcData.temperature
         },
         {
             type: "spline",
@@ -281,139 +280,7 @@ window.onload = function () {
             axisYType: "secondary",
             showInLegend: true,
             color: "#00BFFF",
-            dataPoints: humidityPC_data
-        }],
-        options: {
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    });
-
-    var sensorChart = new CanvasJS.Chart("sensorChart", {
-        //exportEnabled: true,
-        animationEnabled: true,
-        backgroundColor: "",
-        title: {
-            text: "Temperature",
-            fontColor: "#999999"
-        },
-        axisX: {
-            lableAngle: -50,
-            valueFormatString: "DD HH:mm",
-            labelFontColor: "#fffff0"
-        },
-        axisY: [{
-            title: "Base Temperature",
-            suffix: "°C",
-            lineColor: "#CD7045",
-            tickColor: "#CD7045",
-            labelFontColor: "#CD7045",
-            titleFontColor: "#999999"
-        }
-        ],
-        axisY2: [{
-            title: "PC Temperature",
-            suffix: "°C",
-            lineColor: "#993300",
-            tickColor: "#993300",
-            labelFontColor: "#993300",
-            titleFontColor: "#999999"
-        }],
-        toolTip: {
-            shared: true
-        },
-        legend: {
-            cursor: "pointer",
-            itemclick: toggleDataSeries,
-            fontColor: "#999999"
-        },
-        data: [{
-            type: "spline",
-            name: "BME680",
-            connectNullData: true,
-            //nullDataLineDashType: "solid",
-            xValueType: "dateTime",
-            xValueFormatString: "HH:mm DD/MMMM",
-            yValueFormatString: "#,###",
-            axisYIndex: 0,
-            showInLegend: true,
-            color: "#CD7045",
-            dataPoints: temperatureBase_data
-        },
-        {
-            type: "spline",
-            name: "DHT22",
-            connectNullData: true,
-            axisYIndex: 1,
-            axisYType: "secondary",
-            showInLegend: true,
-            color: "#993300",
-            dataPoints: temperaturePC_data
-        }],
-        options: {
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    });
-    var airqualityChart = new CanvasJS.Chart("airqualityChart", {
-        //exportEnabled: true,
-        animationEnabled: true,
-        backgroundColor: "",
-        title: {
-            text: "Air Quality",
-            fontColor: "#999999"
-        },
-        axisX: {
-            lableAngle: -50,
-            valueFormatString: "DD HH:mm",
-            labelFontColor: "#fffff0"
-        },
-        axisY: [{
-            title: "CO2 Concentration",
-            suffix: "ppm",
-            lineColor: "#595959",
-            tickColor: "#595959",
-            labelFontColor: "#595959",
-            titleFontColor: "#999999"
-        }
-        ],
-        axisY2: [{
-            title: "Air Resistance",
-            suffix: "",
-            lineColor: "#737373",
-            tickColor: "#737373",
-            labelFontColor: "#737373",
-            titleFontColor: "#999999"
-        }],
-        toolTip: {
-            shared: true
-        },
-        legend: {
-            cursor: "pointer",
-            itemclick: toggleDataSeries,
-            fontColor: "#999999"
-        },
-        data: [{
-            type: "spline",
-            name: "CO2",
-            connectNullData: true,
-            xValueType: "dateTime",
-            xValueFormatString: "HH:mm DD/MMMM",
-            yValueFormatString: "#,###",
-            axisYIndex: 0,
-            showInLegend: true,
-            color: "#595959",
-            dataPoints: co2_data
-        },
-        {
-            type: "spline",
-            name: "Resistance",
-            connectNullData: true,
-            axisYType: "secondary",
-            axisYIndex: 1,
-            showInLegend: true,
-            color: "#737373",
-            dataPoints: resistance_data
+            dataPoints: pcData.humidity
         }],
         options: {
             responsive: true,
@@ -429,90 +296,89 @@ window.onload = function () {
         renderCharts();
     }
     function addBaseData(data) {
-        // clear previous data
-        while (temperatureBase_data.length > 0) {
-            temperatureBase_data.pop();
-            humidityBase_data.pop();
-            co2_data.pop();
-            resistance_data.pop();
-            brightness_data.pop();
-
-            // temp solution
-            temperaturePC_data.pop();
-            humidityPC_data.pop();
-        }
+        // clear previous
+        baseData = { temperature: [], humidity: [], co2: [], brightness: [] }
         for (var i = 0; i < data.length; i++) {
+            // canvasjs wants timestamps with milliseconds
             data[i].time = data[i].time * 1000;
-            temperatureBase_data.push({
+            baseData.temperature.push({
                 x: new Date(data[i].time),
                 y: data[i].temperature
             });
-            humidityBase_data.push({
+            baseData.humidity.push({
                 x: new Date(data[i].time),
                 y: data[i].humidity
             });
-            co2_data.push({
+            baseData.co2.push({
                 x: new Date(data[i].time),
                 y: data[i].co2
             });
-            resistance_data.push({
-                x: new Date(data[i].time),
-                y: data[i].quality
-            });
-            brightness_data.push({
+            baseData.brightness.push({
                 x: new Date(data[i].time),
                 y: data[i].brightness
-            });
-
-            // temp solution
-            temperaturePC_data.push({
-                x: new Date(data[i].time),
-                y: (Math.random() * (30 - 10) + 10)
-            });
-            humidityPC_data.push({
-                x: new Date(data[i].time),
-                y: (Math.random() * (100 - 40) + 40)
             });
         }
     }
     function addPcData(data) {
-        // clear previous data
-        while (temperaturePC_data.length > 0) {
-            temperaturePC_data.pop();
-            humidityPC_data.pop();
-        }
+        pcData = { temperature: [], humidity: [] }
         for (var i = 0; i < data.length; i++) {
             data[i].time = data[i].time * 1000;
-            temperaturePC_data.push({
+            pcData.temperature.push({
                 x: new Date(data[i].time),
-                y: (Math.random() * (30 - 10) + 10)
+                y: data[i].temperature
             });
-            humidityPC_data.push({
+            pcData.humidity.push({
                 x: new Date(data[i].time),
-                y: (Math.random() * (100 - 40) + 40)
+                y: data[i].humidity
             });
         }
+    }
+    function addTentData(data) {
+        tentData = { temperature: [], humidity: [], co2: [], brightness: [], resistance: [] }
+        for (var i = 0; i < data.length; i++) {
+            data[i].time = data[i].time * 1000;
+            tentData.temperature.push({
+                x: new Date(data[i].time),
+                y: data[i].temperature
+            });
+            tentData.humidity.push({
+                x: new Date(data[i].time),
+                y: data[i].humidity
+            });
+            tentData.co2.push({
+                x: new Date(data[i].time),
+                y: data[i].co2
+            });
+            tentData.brightness.push({
+                x: new Date(data[i].time),
+                y: data[i].brightness
+            });
+            tentData.resistance.push({
+                x: new Date(data[i].time),
+                y: data[i].resistance
+            });
+        }
+
     }
     function setupCharts() {
         //$.getJSON(pathToServer, addBaseData);
         $.getJSON(pathToBase, addBaseData);
-        //$.getJSON(pathToPc, addPcData);
+        $.getJSON(pathToPc, addPcData);
+        $.getJSON(pathToTent, addTentData);
         //setInterval($.getJSON(pathToServer, addData), 60000);
 
         renderCharts();
     }
     function renderCharts() {
-        bigChart.render();
         baseChart.render();
-        sensorChart.render();
-        airqualityChart.render();
+        tentChart.render();
         pcChart.render();
     }
     checkWebsiteStatus('http://baseModule.local/isLive', document.getElementById('baseStatus'));
     checkWebsiteStatus('http://ledModule.local/isLive', document.getElementById('ledStatus'));
     checkWebsiteStatus('http://lightingModule.local/isLive', document.getElementById('lightingStatus'));
     checkWebsiteStatus('http://pcModule.local/isLive', document.getElementById('pcStatus'));
-    checkWebsiteStatus('http://displayModule.local/isLive', document.getElementById('displayStatus'));
+    checkWebsiteStatus('http://tentModule.local/isLive', document.getElementById('tentStatus'));
     checkWebsiteStatus('http://camModule.local/isLive', document.getElementById('camStatus'));
     setupCharts();
     initWebSocket();
